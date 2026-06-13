450ea1246af922113c8ffece504755517504ebb9
Mohammed Baqhir R <baqhirmohammed@gmail.com>
2026-06-13 16:08:47 +0530

    Update general routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,68 +9,69 @@ const isValid = (username) => {
   let userswithsamename = users.filter((user) => {
     return user.username === username;
   });
+
   return userswithsamename.length > 0;
 };
 
-// Get all books
+// Task 2: Get all books
 public_users.get('/', function (req, res) {
   return res.status(200).json(books);
 });
 
-// Get book by ISBN
+// Task 3: Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
     return res.status(200).json(books[isbn]);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
   }
+
+  return res.status(404).json({ message: "Book not found" });
 });
 
-// Get books by author
+// Task 4: Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author.toLowerCase();
 
-  const result = Object.values(books).filter(book =>
-    book.author.toLowerCase() === author
-  );
+  const result = Object.values(books).filter((book) => {
+    return book.author.toLowerCase() === author;
+  });
 
   if (result.length > 0) {
     return res.status(200).json(result);
-  } else {
-    return res.status(404).json({ message: "No books found by this author" });
   }
+
+  return res.status(404).json({ message: "No books found by this author" });
 });
 
-// Get books by title
+// Task 5: Get book details based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title.toLowerCase();
 
-  const result = Object.values(books).filter(book =>
-    book.title.toLowerCase() === title
-  );
+  const result = Object.values(books).filter((book) => {
+    return book.title.toLowerCase() === title;
+  });
 
   if (result.length > 0) {
     return res.status(200).json(result);
-  } else {
-    return res.status(404).json({ message: "No books found with this title" });
   }
+
+  return res.status(404).json({ message: "No books found with this title" });
 });
 
-// Get reviews by ISBN
+// Task 6: Get book reviews
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   if (books[isbn]) {
     return res.status(200).json(books[isbn].reviews);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
   }
+
+  return res.status(404).json({ message: "Book not found" });
 });
 
-// Register new user
-public_users.post('/register', (req, res) => {
+// Task 7: Register new user
+public_users.post('/register', function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -90,7 +91,7 @@ public_users.post('/register', (req, res) => {
   return res.status(200).json({ message: "User successfully registered" });
 });
 
-// Async/Await: Get all books
+// Task 11: Get all books using async/await
 public_users.get('/async/books', async function (req, res) {
   try {
     const getBooks = () => {
@@ -106,7 +107,7 @@ public_users.get('/async/books', async function (req, res) {
   }
 });
 
-// Promise: Search book by ISBN
+// Task 11: Search book by ISBN using Promise
 public_users.get('/async/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
@@ -119,18 +120,22 @@ public_users.get('/async/isbn/:isbn', function (req, res) {
   });
 
   getBookByISBN
-    .then(result => res.status(200).json(result))
-    .catch(error => res.status(404).json({ message: error }));
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: error });
+    });
 });
 
-// Promise: Search books by author
+// Task 11: Search books by author using Promise
 public_users.get('/async/author/:author', function (req, res) {
   const author = req.params.author.toLowerCase();
 
   const getBooksByAuthor = new Promise((resolve, reject) => {
-    const result = Object.values(books).filter(book =>
-      book.author.toLowerCase() === author
-    );
+    const result = Object.values(books).filter((book) => {
+      return book.author.toLowerCase() === author;
+    });
 
     if (result.length > 0) {
       resolve(result);
@@ -140,20 +145,24 @@ public_users.get('/async/author/:author', function (req, res) {
   });
 
   getBooksByAuthor
-    .then(result => res.status(200).json(result))
-    .catch(error => res.status(404).json({ message: error }));
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((error) => {
+      return res.status(404).json({ message: error });
+    });
 });
 
-// Async/Await: Search books by title
+// Task 11: Search books by title using async/await
 public_users.get('/async/title/:title', async function (req, res) {
   const title = req.params.title.toLowerCase();
 
   try {
     const getBooksByTitle = () => {
       return new Promise((resolve, reject) => {
-        const result = Object.values(books).filter(book =>
-          book.title.toLowerCase() === title
-        );
+        const result = Object.values(books).filter((book) => {
+          return book.title.toLowerCase() === title;
+        });
 
         if (result.length > 0) {
           resolve(result);
